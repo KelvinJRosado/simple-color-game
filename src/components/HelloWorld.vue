c479d029c936f9a85c61f9124674cedc48dcbcd0
KelvinJRosado <kelvinrosado1@yahoo.com>
2025-06-21 16:27:38 -0400

    Reset to no styling

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,7 @@
-<script setup lang="ts">
-// Composition API, no props needed
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <h1>Hello world</h1>
 </template>
 
-<style scoped>
-h1 {
-  color: #42b983;
-  text-align: center;
-  margin-top: 2rem;
-}
-</style>
+<style scoped></style>
